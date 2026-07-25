fbcfb6a15e6a31d3c6626ddbe74d6dd40a3c939a
Nikhil <starnikhil04@gmail.com>
2026-07-25 12:17:47 +0530

    configure Rojo and Vs code ; learn print and comments

--- src/ServerScriptService/Main.server.lua
+++ src/ServerScriptService/Main.server.lua
@@ -1,1 +1,3 @@
-print("hello roblox")
+print("So rojo server is working properly")  -- So this is first print statement is used to check whether the link between vs code and roblox studio is formed or not.
+-- As we can see if we type the command "rojo serve" in git bash and connect it with rojo plugin in roblox studio the code is syncing properly.
+-- that means configuration of rojo and VS code is been formed succesfully.
